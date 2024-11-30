0d16342f1e4561f6a32b4de2fc7aca2435ba97be
mingssii <147397860+mingssii@users.noreply.github.com>
2024-11-30 23:12:14 +0700

    Update arduino.cpp

--- src/arduino.cpp
+++ src/arduino.cpp
@@ -1,15 +1,7 @@
-#include <DHT.h>
-
-#define DHTPIN 2
-#define DHTTYPE DHT11
-
-DHT dht(DHTPIN, DHTTYPE);
-
-// Soil Moisture Sensor
-#define SOIL_AO 4
+#include <Arduino.h>
 
 //Rain Sensor
-#define RAIN_DO A0   // ขา Analog
+#define RAIN_AO A0   // ขา Analog
 
 // กำหนดขา Liquid Level Sensor
 #define LIQUID_SENSOR_PIN 3  // ขา Digital
@@ -18,53 +10,38 @@ const int sensorMin = 0;     // sensor minimum
 const int sensorMax = 1024;  // sensor maximum
 
 void setup() {
-    Serial.begin(115200); // UART
-    dht.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
-    float humidity = dht.readHumidity();
-    float temperature = dht.readTemperature();
-    int soilMoisture = analogRead(SOIL_AO);
-    // ส่งข้อมูลในรูปแบบ JSON
-    Serial.print("{");
-    Serial.print("\"humidity\":");
-    Serial.print(humidity);
-    Serial.print(",\"temperature\":");
-    Serial.print(temperature);
-    Serial.println("}");
+  // อ่านค่าจาก Raindrop Sensor
+  int sensorReading = analogRead(RAIN_AO);
+  // map the sensor range (four options):
+  // ex: 'long int map(long int, long int, long int, long int, long int)'
+  int rainStatus = map(sensorReading, sensorMin, sensorMax, 0, 3);
+  // Serial.print("Rain Analog Status: ");
+  // Serial.print(range);
+  // Serial.print(" - ");
 
+  // // range value:
+  // switch (range) {
+  // case 0:    // Sensor getting wet
+  //   Serial.println("Rain Warning");
+  //   break;
+  // default:
+  //   Serial.println("Not Raining");
+  // }
 
-    // อ่านค่าจาก Raindrop Sensor
-    int sensorReading = analogRead(RAIN_DO);
-    // map the sensor range (four options):
-    // ex: 'long int map(long int, long int, long int, long int, long int)'
-    int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
-    Serial.print("Rain Analog Status: ");
-    Serial.print(range);
-    Serial.print(" - ");
-    
-    // range value:
-    switch (range) {
-        case 0:    // Sensor getting wet
-        Serial.println("Rain Warning");
-        break;
-        default:
-        Serial.println("Not Raining");
-    }
+  // อ่านค่าจาก Liquid Level Sensor
+  int liquidStatus = digitalRead(LIQUID_SENSOR_PIN);
 
-    // อ่านค่าจาก Liquid Level Sensor
-    int liquidStatus = digitalRead(LIQUID_SENSOR_PIN);
-    Serial.print("Liquid Level Sensor Status: ");
-    Serial.print(liquidStatus);  // Print the value of liquidStatus
-    Serial.print(" - ");
-    if (liquidStatus == 1) {
-        Serial.println("Liquid detected!");
-    } else {
-        Serial.println("No liquid detected.");
-    }
-    
-    delay(2000); // ส่งข้อมูลทุก 2 วินาที
+  // ส่งข้อมูลในรูปแบบ JSON
+  Serial.print("{");
+  Serial.print(rainStatus);
+  Serial.print(",");
+  Serial.print(liquidStatus);
+  Serial.println("}");
+  delay(3000); // ส่งข้อมูลทุก 5 วินาที
 
 
 }
